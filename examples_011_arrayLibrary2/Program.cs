03b3faac4dd3a3ab7b3b3b75146216f3f514140c
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-18 00:31:45 +0200

    new methods added

---
+++ examples_011_arrayLibrary2/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    // tozhe cto i index
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int indexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    //-1 чтобы в случае, если элемента нет, 
+    //то он будет выдавать -1, а не 0
+    //проверка на наличие 
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = indexOf(array, 14);
+Console.WriteLine(pos);
