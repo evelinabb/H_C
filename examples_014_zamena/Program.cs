04464b6e14549d32ec80316e3dc43bb9611b9715
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-30 00:55:08 +0200

    added 14 example

---
+++ examples_014_zamena/Program.cs
@@ -0,0 +1,27 @@
+﻿string text = "аврвлрлж к роаралпр с"
+            + "ФЫЛОЛЭФВЧ К ЫФДЖЛОЦ С";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
