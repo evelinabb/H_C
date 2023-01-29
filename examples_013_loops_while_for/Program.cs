e5116fc3e6a77ea507015546d68199a6e337475a
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-30 00:37:46 +0200

    added 13 example

---
+++ examples_013_loops_while_for/Program.cs
@@ -0,0 +1,11 @@
+﻿string MethodForFor (int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i<count; i++) //inicialiyzcia scetcika; proverka uslovia; inkrement
+    {
+        result +=text;
+    }
+    return result;
+}
+string res = MethodForFor(10, "z");
+Console.WriteLine(res);
