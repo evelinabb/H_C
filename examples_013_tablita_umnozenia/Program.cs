dba8a5ad59923b7f29d3fd2e07feccc030c8ef5b
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-30 00:41:54 +0200

    added second 13 example

---
+++ examples_013_tablita_umnozenia/Program.cs
@@ -0,0 +1,8 @@
+﻿for (int i = 2; i < 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}"); // interpoliatsia strok
+    }
+    Console.WriteLine(); //delaetsia razryv tipa novaya stroka
+}
